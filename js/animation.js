8c6c3f10aac9e966e62a29403cf2e8f839bfe1bc
bmbekesi <152105472+bmbekesi@users.noreply.github.com>
2024-03-11 00:18:09 +0100

    Add files via upload

---
+++ js/animation.js
@@ -0,0 +1,32 @@
+// Get the animation element
+var elem = document.getElementById("myAnimation");
+
+// Set the initial position
+var pos_x = 150;
+var pos_y = 150;
+
+window.addEventListener("mousemove", function(event) {
+  // Get the mouse coordinates from the event object
+  var x = event.clientX;
+  var y = event.clientY;
+
+
+// Define the animation function
+function myMove() {
+  // Increment the position by 1 pixel
+  pos_x = pos_x + 5*Math.sign(Math.random()-0.5)+x;
+  pos_y = pos_y + 5*Math.sign(Math.random()-0.5)+y;
+  // Update the style properties
+  elem.style.top = pos_x + "px";
+  elem.style.left = pos_y + "px";
+  // Check if the position reaches the end of the container
+  if (pos_x == 350) {
+    // Reset the position to the start
+    pos_x = 0;
+  }
+  // Request the next frame of the animation
+  requestAnimationFrame(myMove);
+}
+
+// Start the animation
+myMove();
